6ed653dfb02a3aa432c6ba90b8d809ba6f37eb38
Noah Zoschke <noah@heroku.com>
2012-06-21 18:29:54 -0700

    latest slide controls

--- gistdeck.js
+++ gistdeck.js
@@ -1,27 +1,40 @@
-var docs = "Turn into Bookmarklet at http://mcdlr.com/js-inject/";
-
 if (typeof(GISTDECK_CSS_URL) == "undefined")
     var GISTDECK_CSS_URL="https://gistdeck.herokuapp.com/gistdeck.css"
-
 $("head").append('<link rel="stylesheet" href="' + GISTDECK_CSS_URL + '" type="text/css" />');
 
-var slideNum = 0;
+var slides = $("#owner, .markdown-body h1, .markdown-body h2");
 
-function setSlide(idx) {
-  var slides = $(".markdown-body h1, .markdown-body h2");
+function getCurrentSlideIdx() {
+  var idx = 0;
+  var viewportBottom = $(window).scrollTop() + $(window).height();
 
-  idx = Math.min(idx, slides.length-1);
-  idx = Math.max(idx, 0);
-  console.log(idx);
+  for (var i=0; i < slides.length; i++) {
+    if (slides.eq(i).offset().top > viewportBottom) break;
+    idx = i;
+  }
 
-  var s = $(slides[idx]);
-  $("body").animate({scrollTop: s.offset().top}, 50);
-  slideNum = idx;
+  return idx;
+}
+
+function displaySlide(n) {
+  n = Math.min(n, slides.length-1);
+  n = Math.max(n, 0);
+
+  var s = slides.eq(n);
+  var top = s.offset().top;
+
+  var padding = {
+    "DIV": s.offset().top,
+    "H1":  150,
+    "H2":  20
+  }[slides[n].tagName];
+
+  $("body").scrollTop(top - padding);
 }
 
 $(document).keydown(function(e) {
-  if (e.which == 37)      setSlide(slideNum-1);
-  else if (e.which == 39) setSlide(slideNum+1);
+  if (e.which == 37)      displaySlide(getCurrentSlideIdx()-1);
+  else if (e.which == 39) displaySlide(getCurrentSlideIdx()+1);
 });
 
-setSlide(0);
+displaySlide(0);
